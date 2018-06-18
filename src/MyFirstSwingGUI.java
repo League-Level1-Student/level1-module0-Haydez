b9fcb3e713b8d3533d2a43effa3ad2a3558cc69b
league <league@wts8.attlocal.net>
2018-06-17 17:30:16 -0700

    NEET my first swing

--- src/MyFirstSwingGUI.java
+++ src/MyFirstSwingGUI.java
@@ -10,36 +10,37 @@ import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class MyFirstSwingGUI {
 	public static void main(String[] args) {
 		//1. Create and initialize an object of the JFrame class
-
+		JFrame frame = new JFrame();
 		//2. Set your JFrame object to be visible
-
+		frame.setVisible(true);
 		//3. Run your program. Do you see your window? It's probably very small.
-		
+		frame.setSize(1000, 1000);
 		//4. Set the default close operation to JFrame.EXIT_ON_CLOSE
-
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		//5. Create and initialize an object of the JPanel class
-
+		JPanel panel = new JPanel();
 		//6. Create and initialize an object of the JLabel class
-
+		JLabel Label = new JLabel();
 		//7. Set the text of the JLabel to a lovely greeting.
-
+		Label.setText("Henlo dere");
 		//8. Add the JPanel object to the JFrame
-
+		frame.add(panel);
 		//9. Add the JLabel object to the JPanel
-
+		panel.add(Label);
 		//10. Pack your JFrame.
-
+		
 		//11. Run your program again. Do you see your message.
 		
 		//12. Use the loadImage method to set the icon of the JLabel object.  
-
+		Label.setIcon(loadImage());
 		//13. Re-pack the JFrame object.
-		
+		frame.pack();
 		//14. Run the program one more time. Do you see the image?
 	}
 	
